6b1516868b65344ae2e31ee34ae2dd9e2f3f0a9e
ismail-nabaji <91204944+ismail-nabaji@users.noreply.github.com>
2023-04-18 18:10:03 +0200

    mise en place interface

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
+  const [player, setPlayer] = useState('X');
+  const [gameStatus, setGameStatus] = useState('unstarted');
+
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Tic Tac Toe</h1>
+        <div className="board">
+          {board.map((value, index) => (
+            <div>
+              <button className="square" onClick={() => {}}>
+                X
+              </button>
+
+            </div>))}
+          </div>
       </header>
     </div>
   );
