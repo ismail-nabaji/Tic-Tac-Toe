6abec94902f5e186bfe76f9f266b2328cf01f167
ismail-nabaji <91204944+ismail-nabaji@users.noreply.github.com>
2023-04-19 12:21:07 +0200

    fix beug (on ne peu plus jouer apres avoir fini la partie)

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
   
 
   const makeMove = (index) => {
-    if (gameStatus !== 'unstarted') {
+    if (gameStatus !== 'stoped') {
       setGameStatus('started');
-    }
+    
       console.log(index);
       console.log(player);
       console.log(board);
@@ -26,35 +26,35 @@ function App() {
         alert('This square is already taken');
       }
     }
-  
+  }
 
   const checkWinner = () => {
     if (board[0] === board[1] && board[1] === board[2] && board[0] !== ''){
-      setGameStatus('winner');
+      setGameStatus('stoped');
       alert('Player ' + player + ' wins!');
     }else if (board[3] === board[4] && board[4] === board[5] && board[3] !== ''){
-      setGameStatus('winner');
+      setGameStatus('stoped');
       alert('Player ' + player + ' wins!');
     }else if (board[6] === board[7] && board[7] === board[8] && board[6] !== ''){
-      setGameStatus('winner');
+      setGameStatus('stoped');
       alert('Player ' + player + ' wins!');
     }else if (board[0] === board[3] && board[3] === board[6] && board[0] !== ''){
-      setGameStatus('winner');
+      setGameStatus('stoped');
       alert('Player ' + player + ' wins!');
     }else if (board[1] === board[4] && board[4] === board[7] && board[1] !== ''){
-      setGameStatus('winner');
+      setGameStatus('stoped');
       alert('Player ' + player + ' wins!');
     }else if (board[2] === board[5] && board[5] === board[8] && board[2] !== ''){
-      setGameStatus('winner');
+      setGameStatus('stoped');
       alert('Player ' + player + ' wins!');
     }else if (board[0] === board[4] && board[4] === board[8] && board[0] !== ''){
-      setGameStatus('winner');
+      setGameStatus('stoped');
       alert('Player ' + player + ' wins!');
     }else if (board[2] === board[4] && board[4] === board[6] && board[2] !== ''){
-      setGameStatus('winner');
+      setGameStatus('stoped');
       alert('Player ' + player + ' wins!');
     }else if (board[0] !== '' && board[1] !== '' && board[2] !== '' && board[3] !== '' && board[4] !== '' && board[5] !== '' && board[6] !== '' && board[7] !== '' && board[8] !== ''){
-      setGameStatus('draw');
+      setGameStatus('stoped');
       alert('Draw!');
     }
   }
